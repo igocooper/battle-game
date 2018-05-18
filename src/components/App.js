202785fbd76ec267aafbc46d994076868934e43b
Oleg Lytvyn <igocooper@gmail.com>
2018-05-18 13:55:52 +0300

    init setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        Hello. I'm App.
+      </div>
+    );
+  }
+}
+
+export default App;
